6b6874ba3284fe2eec349c77be8beeb4c7588a68
Fatih <127442816+FatihAkinn@users.noreply.github.com>
2023-06-05 23:37:03 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/models/todo.dart';
+import 'package:myapp/screens/todolist.dart';
+import 'package:myapp/screens/todolistModel2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      //home: const TodoListModel(),
+      initialRoute: "/",
+      routes: {
+        "/":(context) =>  const TodoList(),
+        "todolistmodel":(context) => const TodoListModel(),
+      },
+    );
+  }
+}
+
